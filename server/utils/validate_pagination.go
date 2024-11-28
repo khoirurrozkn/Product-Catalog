4a7d015dcee7015d51e42d913cfead67f5726154
khoirurrozkn <khoirurrozkn@gmail.com>
2024-11-28 17:36:47 +0700

    [ADD] validate query param for get pagination items

---
+++ server/utils/validate_pagination.go
@@ -0,0 +1,36 @@
+package utils
+
+import (
+	"fmt"
+	"challenge_bioskop/model/dto"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ValidateRequestQueryParams(c *gin.Context) (dto.RequestQueryParam, error) {
+
+	page, err := strconv.Atoi(c.DefaultQuery("page", "1"))
+	if err != nil || page < 0 {
+		return dto.RequestQueryParam{}, fmt.Errorf("Invalid page number")
+	}
+
+	limit, err := strconv.Atoi(c.DefaultQuery("limit", "5"))
+	if err != nil || limit <= 0 {
+		return dto.RequestQueryParam{}, fmt.Errorf("Invalid limit number")
+	}
+
+	order := c.DefaultQuery("order", "name")
+	sort := c.DefaultQuery("sort", "asc")
+
+	return dto.RequestQueryParam{
+		QueryParams: dto.QueryParams{
+			Order: order,
+			Sort: sort,
+		},
+		PaginationParam: dto.PaginationParam{
+			Page: page,
+			Limit: limit,
+		},
+	}, nil
+}
